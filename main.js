3cf3c5a7e99b41a24dc3b7b126ffe7f66c70c49f
naiiiden <na4ko359@gmail.com>
2024-06-26 11:55:40 +0300

    remove clock :D

--- main.js
+++ main.js
@@ -12,18 +12,6 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-function updateClock() {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-  time.textContent = `${hours}:${minutes}:${seconds}`;
-};
-
-updateClock();
-
-setInterval(updateClock, 1000);
-
 const container = document.getElementById('threejs-container');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
